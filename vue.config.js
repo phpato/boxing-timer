075a4bd7dbfe9d98e8b3b7560c57232783dbffaf
patricio <patricio.cabrera@externos-cl.cencosud.com>
2024-12-08 17:12:14 -0300

    vue config nuevo

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  
+  // Configura el publicPath si es necesario
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  
+  // Configura la optimización
+  configureWebpack: {
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+      },
+    },
+  },
+  
+  // Otras configuraciones que desees agregar
+});
